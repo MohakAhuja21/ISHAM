2f5cb3a8f2ab89173df27c25fd452738f3c5e132
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-12-22 16:34:51 +0530

    added Top Loader but is not working..

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import {useEffect} from 'react';
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import { auth } from './firebase';
-import Checkout from './Checkout';
-import {useStateValue} from "./StateProvider";
+import { auth } from "./firebase";
+import Checkout from "./Checkout";
+import Loader from "./Loader";
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  const [{},dispatch]=useStateValue();
+  const [{}, dispatch] = useStateValue();
 
-  useEffect(()=>{
-    auth.onAuthStateChanged(authUser =>{
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
       // console.log("the user is ",authUser);
       if (authUser) {
         // user was logged in/ user logged in
         dispatch({
-          type:'SET_USER',
-          user:authUser
-        })
-      }
-      else{
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
         // user is logged out
         dispatch({
-          type:'SET_USER',
-          user:null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  },[])
+    });
+  }, []);
 
   return (
     <Router>
       <div className="app">
+        {/* TOP LOADER */}
+        <Loader />
         <Routes>
-        <Route path="/checkout" element={[<Header/>,<Checkout/>]}/>
-        <Route path="/login" element={[<Login/>]}/>
-        <Route path="/" element={[<Header/>,<Home/>]}/>
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/login" element={[<Login />]} />
+          <Route path="/" element={[<Header />, <Home />]} />
         </Routes>
       </div>
     </Router>
--- src/Login.js
+++ src/Login.js
@@ -3,19 +3,39 @@ import { Link, useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
 import "./Login.css";
 import logo from "../src/source/ISHAM.svg";
+// TOP LOADER
+import { useStateValue } from "./StateProvider";
+
 
 function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // TOP LOADER
+  const [{}, dispatch] = useStateValue();
 
   const signIn = (e) => {
     // prevent default will prevent the page from refreshing.
     e.preventDefault();
     // linking firebase//
+
+    // TOP LOADER
+    dispatch({
+      type: "CHANGE_LOADER",
+      loader: true,
+      loader_status: 40,
+    });
+
     auth
       .signInWithEmailAndPassword(email, password)
       .then((auth) => {
+        // TOP LOADER
+        dispatch({
+          type: "CHANGE_LOADER",
+          loader: true,
+          loader_status: 100,
+        });
+
         navigate("/");
       })
       .catch((error) => alert(error.message));
@@ -24,11 +44,27 @@ function Login() {
     e.preventDefault();
     // linking firebase//
     // it will create a user with that user and password and will come back with auth object.
+
+    // TOP LOADER
+    dispatch({
+      type: "CHANGE_LOADER",
+      loader: true,
+      loader_status: 40,
+    });
+
     auth
       .createUserWithEmailAndPassword(email, password)
       .then((auth) => {
         // successfully created new user with provided email and pass.
         // console.log(auth);
+
+        // TOP LOADER
+        dispatch({
+          type: "CHANGE_LOADER",
+          loader: true,
+          loader_status: 100,
+        });
+
         if (auth) {
           navigate("/");
         }
